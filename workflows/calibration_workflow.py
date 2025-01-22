de768d1c08ef68c9ec429558b03534139506b105
owen-melville <157226448+owen-melville@users.noreply.github.com>
2025-01-22 18:20:33 -0500

    Calibration data for multi-dispense (constant v)

--- workflows/calibration_workflow.py
+++ workflows/calibration_workflow.py
@@ -11,12 +11,11 @@ import numpy as np
 #export_directory = Path("C:\Users\Imaging Controller\Desktop\utoronto_demo\Calibration Data\Calibration Jan 2025") #edit
 
 
-EXPORT_FILEPATH = r"C:\Users\Imaging Controller\Desktop\utoronto_demo\Calibration Data\Calibration Jan 2025\calibration_water_250_dispenseall_2.txt" #put solvent, dispense_method, into notes
+EXPORT_FILEPATH = r"C:\Users\Imaging Controller\Desktop\utoronto_demo\Calibration Data\Calibration Jan 2025\calibration_water_250_multi_constant.txt" #put solvent, dispense_method, into notes
 #ASPIRATE_VOLUMES = [0.75, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05]
-ASPIRATE_VOLUMES = [0.04, 0.03, 0.02, 0.01]
+#ASPIRATE_VOLUMES = [0.04, 0.03, 0.02, 0.01]
+ASPIRATE_VOLUMES = [0.075, 0.05, 0.025, 0.01, 0.005]
 REPLICATES = 3
-
-ASPIRATE_VOLUMES_MULTI = [[1, 0.5, 0.2, 0.15, 0.1, 0.050, 0.025]]
 
 
 def consistent_v_multi(volumes, replicates):
@@ -28,6 +27,9 @@ def consistent_v_multi(volumes, replicates):
             
         multi_volumes.append(temp_volumes)
     return multi_volumes
+
+ASPIRATE_VOLUMES_MULTI = consistent_v_multi([0.075, 0.05, 0.025, 0.01, 0.005], 3)
+
 
 
 def dispense_all_calibration(input_vial_status_file, source_vial, aspirate_volumes, replicates, dest_vial_position, export_path, buffer = 0):
@@ -84,6 +86,7 @@ def multi_dispense_calibration(input_vial_status_file, source_vial, aspirate_vol
   
     vial_status = pd.read_csv(input_vial_status_file, sep=r"\t", engine="python")
     print(vial_status)
+    print(aspirate_volume_list)
 
     input("Only hit enter if the status of the vials (including open/close) is correct, otherwise hit ctrl-c")
 
@@ -93,7 +96,7 @@ def multi_dispense_calibration(input_vial_status_file, source_vial, aspirate_vol
 
     try:
         #This uncaps our target vessel to receive liquids in the clamp
-        lash_e.nr_robot.c9.DEFAULT_VEL(20)
+        lash_e.nr_robot.c9.DEFAULT_VEL= 20
         lash_e.nr_robot.move_vial_to_clamp(dest_vial_position)
         lash_e.nr_robot.uncap_clamp_vial()
 
@@ -111,8 +114,8 @@ def multi_dispense_calibration(input_vial_status_file, source_vial, aspirate_vol
                 print(f"Dispensing {volume_l[i]}: mass = {mass_str}")
                 calibration_data.append([volume_l[i], mass_str]) 
 
-                if buffer > 0:
-                    lash_e.nr_robot.dispense_into_vial(source_vial, amount_mL=buffer) #dispense extra buffer back into source
+            if buffer > 0:
+                lash_e.nr_robot.dispense_into_vial(source_vial, amount_mL=buffer) #dispense extra buffer back into source
 
         
         lash_e.nr_robot.remove_pipet()
@@ -120,6 +123,7 @@ def multi_dispense_calibration(input_vial_status_file, source_vial, aspirate_vol
         #Remove the pipet and return the vial if needed
         lash_e.nr_robot.finish_pipetting()
 
+        lash_e.nr_robot.c9.move_z(292)
         np.savetxt(export_path, calibration_data,delimiter='\t', fmt ='% s') #export file
 
     
@@ -127,4 +131,4 @@ def multi_dispense_calibration(input_vial_status_file, source_vial, aspirate_vol
         lash_e = None
     
 
-multi_dispense_calibration(input_vial_status_file="../utoronto_demo/status/vial_status_wellplate - test.txt", source_vial = 0, aspirate_volume_list=ASPIRATE_VOLUMES_MULTI, replicates=REPLICATES , dest_vial_position=1, export_path = EXPORT_FILEPATH, buffer=0)
+multi_dispense_calibration(input_vial_status_file="../utoronto_demo/status/vial_status_wellplate - test.txt", source_vial = 0, aspirate_volume_list=ASPIRATE_VOLUMES_MULTI, dest_vial_position=1, export_path = EXPORT_FILEPATH, buffer=0)
