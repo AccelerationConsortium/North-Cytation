88faa13b57513ec7c0892dcd44ae0968f0d9ba8d
owen-melville <157226448+owen-melville@users.noreply.github.com>
2025-01-30 12:47:30 -0500

    update

--- analysis/spectral_difference.py
+++ analysis/spectral_difference.py
@@ -4,7 +4,26 @@ import os
 
 #We will need the file data and the labels
 
+def remove_overflow(data):
+    processed_data = []
+    last_valid_value = None
+    for item in data:
+        if item == 'OVRFLW':
+            # If we encounter 'OVRFLW', replace it with the last valid number
+            processed_data.append(last_valid_value)
+        else:
+            # Convert the item to a float and add to the list
+            value = float(item)
+            processed_data.append(value)
+            last_valid_value = value  # Update the last valid value
+    return processed_data
+
 def get_differences(reference_data_file, reference_index, target_data_file, target_index_list):
+
+    #print("Reference file: ", reference_data_file)
+    #print("Reference index", reference_index)
+    #print("Target Data file", target_data_file)
+    #print("Target index ", target_index_list)
 
     ref_data = pd.read_csv(reference_data_file, sep='\t', skiprows=[0,1])
     comp_data = pd.read_csv(target_data_file, sep='\t', skiprows=[0,1])
@@ -14,11 +33,16 @@ def get_differences(reference_data_file, reference_index, target_data_file, targ
 
     ref_spectra = ref_data.iloc[:, reference_index+1].values
 
+    #print("Ref spectra", ref_spectra)
+
     difference = []
     for i in target_index_list:
         if np.all(wavelengths_ref == wavelengths_comp):
             comp_spectra=comp_data.iloc[:,i+1].values
-            difference.append(float(np.sum(ref_spectra-comp_spectra)))
+
+            print(comp_spectra)
+
+            difference.append(float(np.sum(np.array(remove_overflow(ref_spectra))-np.array(remove_overflow(comp_spectra)))))
         else:
             print("Script failed - spectra not comparable")
             difference.append(None)
