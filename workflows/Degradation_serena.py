69a3465196bd2ea4be04adccfee175136d73efa7
owen-melville <157226448+owen-melville@users.noreply.github.com>
2025-10-07 14:04:07 -0400

    asd

--- workflows/Degradation_serena.py
+++ workflows/Degradation_serena.py
@@ -3,7 +3,6 @@ import time
 sys.path.append("../utoronto_demo")
 from master_usdl_coordinator import Lash_E 
 import pandas as pd
-import slack_agent
 import os
 from pathlib import Path
 
@@ -20,7 +19,7 @@ def measure_absorbance(lash_e,reaction_mixture_index,sample_index,volume=0.2):
        # lash_e.nr_robot.home_axis(i) #Home the track
     print()
 
-def measure_absorbance(lash_e,sample_index,first_well_index,cytation_protocol_file_path,replicates,output_dir,simulate=False,well_volume=0.2):
+def measure_absorbance(lash_e,sample_index,first_well_index,cytation_protocol_file_path,replicates,well_volume=0.2):
     print("\nTransferring sample: ", sample_index, " to wellplate at well index: ", first_well_index)
     lash_e.nr_robot.move_vial_to_location(sample_index, location='heater', location_index=2) #only use heater location 2 for safe pipetting
     lash_e.nr_robot.aspirate_from_vial(sample_index, well_volume*replicates,track_height=True)
@@ -29,12 +28,14 @@ def measure_absorbance(lash_e,sample_index,first_well_index,cytation_protocol_fi
     lash_e.nr_robot.remove_pipet()
     lash_e.nr_robot.return_vial_home(sample_index) #Return sample to home position
     data_out = lash_e.measure_wellplate(cytation_protocol_file_path, wells_to_measure=wells)
+    return data_out
+
+def save_data(output_dir, data_out, first_well_index):
     output_file = output_dir / f'output_{first_well_index}.txt'
     # output_file = r'C:\Users\Imaging Controller\Desktop\SQ\output_'+str(first_well_index)+'.txt'
-    if not simulate:
-        output_file = output_dir / f'output_{first_well_index}.txt'
-        data_out.to_csv(output_file, sep=',')
-    print()
+    output_file = output_dir / f'output_{first_well_index}.txt'
+    data_out.to_csv(output_file, sep=',')
+    print("Data saved to:", output_file)
 
 def wash_wellplate(lash_e, first_well_index, new_pipet=False,repeats=3, volume=0.3):
     print("\nWashing well plate: ", first_well_index)
@@ -71,8 +72,8 @@ def peroxide_workflow(reagent_incubation_time=20*60,sample_incubation_time=18*60
     print(vial_status)
 
     #Initialize the workstation, which includes the robot, track, cytation and photoreactors
-    SIMULATE = LOGGING = False #Set to True if you want to simulate the robot, False if you want to run it on the real robot
-    lash_e = Lash_E(INPUT_VIAL_STATUS_FILE, simulate=SIMULATE, logging=LOGGING)
+    SIMULATE = True #Set to True if you want to simulate the robot, False if you want to run it on the real robot
+    lash_e = Lash_E(INPUT_VIAL_STATUS_FILE, simulate=SIMULATE)
 
     #This section is simply to create easier to remember and read indices for the vials
     #vial_numbers = vial_status['vial_index'].values #Gives you the values
@@ -89,6 +90,7 @@ def peroxide_workflow(reagent_incubation_time=20*60,sample_incubation_time=18*60
 
     #create file name for the output data
     if not SIMULATE:
+        import slack_agent
         exp_name = input("Experiment name: ")
         output_dir = Path(r'C:\Users\Imaging Controller\Desktop\SQ') / exp_name #appends exp_name to the output directory
         output_dir.mkdir(parents=True, exist_ok=True)
